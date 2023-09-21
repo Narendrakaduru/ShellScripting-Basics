ac8441e69bade3a54548a0936d3cce67dc94b616
Narendrakaduru <kadurunarendra@gmail.com>
2023-09-21 10:35:23 +0530

    Update Setup_VM.sh

--- Setup_VM.sh
+++ Setup_VM.sh
@@ -55,7 +55,7 @@ source /root/.bashrc
 
 # Add user to Sudoers file
 echo "Add user to Sudoers file"
-echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+echo "$user ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
 # Enable Password Authentication
 echo "Enable Password Authentication"
