0bd5f5883775de666a04bed053764682e658e269
Narendrakaduru <kadurunarendra@gmail.com>
2023-10-15 11:51:14 +0530

    Update Setup_VM.sh

--- Setup_VM.sh
+++ Setup_VM.sh
@@ -64,10 +64,6 @@ echo "Enable Password Authentication"
 sudo echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
 
 # Disable IPv6
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-
 sudo echo "net.ipv6.conf.all.disable_ipv6=1
 net.ipv6.conf.default.disable_ipv6=1
 net.ipv6.conf.lo.disable_ipv6=1" >> /etc/sysctl.conf
